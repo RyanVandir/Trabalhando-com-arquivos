7ad70a044b12d58f9d3a80814a3ae5e2c6a1c09f
RyanVandir <r.vandir@outlook.com>
2024-08-08 17:54:55 -0300

    Processo melhor de ler o arquivo (fechando os recursos no final do bloco de forma altomática)

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in);
 
-        //String[] lines = new String[] {sc.nextLine()};
+        ///String[] lines = new String[] {sc.nextLine()};
 
         String path = "C:\\Users\\rvand\\OneDrive\\CursoUdemy\\Java\\Trabalhando com arquivos\\out.txt";
 
@@ -24,13 +22,10 @@ public class Program {
         } catch (IOException e){
             e.printStackTrace();
         }*/
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        /* É um bloco try que declara um ou mais recursos, e garante que esses
+        recursos serão fechados ao final do bloco */
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -39,17 +34,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
